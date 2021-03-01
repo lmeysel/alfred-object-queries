4c147f0678e056d8eb6dcd8027979d371c338695
lmeysel <ludwig@meysel.de>
2021-03-01 16:35:13 +0100

    properly load global constants in tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,4 @@
-import constants from '../constants'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const constants = require('../constants');
 
 for (const key in constants) (global as unknown as Record<string, number>)[key] = constants[key]
